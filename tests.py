7536562dfdcdd519bfc0c01119c06d8eea30ff51
Adonay <adonay.rssantos@gmail.com>
2025-03-06 19:32:05 -0300

    Testes para mostrar o funcionamento da função de schemas criada

--- tests.py
+++ tests.py
@@ -1,7 +1,14 @@
-from ORM import ORM
+from ORM import DB_ORM
+import json
 
-bd = ORM()
+db = DB_ORM()
 
-bd.get_connection()
+db.get_connection()
 
-print(bd.get_tables())
+print(db.get_tables())
+
+table_departamentos = db.get_table_schema("departamentos")
+table_funcionarios = db.get_table_schema("funcionarios")
+
+print(json.dumps(table_departamentos, indent=4, ensure_ascii=False))
+print(json.dumps(table_funcionarios, indent=4, ensure_ascii=False))
