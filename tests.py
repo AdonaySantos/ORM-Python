3948d9bc7b3fc3e32844147584863ba3634e33ab
Adonay <adonay.rssantos@gmail.com>
2025-03-10 14:30:28 -0300

    Implementação de testes para o get_data

--- tests.py
+++ tests.py
@@ -12,3 +12,10 @@ table_funcionarios = db.get_table_schema("funcionarios")
 
 print(json.dumps(table_departamentos, indent=4, ensure_ascii=False))
 print(json.dumps(table_funcionarios, indent=4, ensure_ascii=False))
+
+data_departamentos = db.get_data("departamentos", ["id_departamento", "nome_departamento"])
+data_funcionarios = db.get_data("funcionarios", ["id_funcionario", "nome_funcionario", "id_departamento"])
+
+print(data_departamentos)
+print()
+print(data_funcionarios)
