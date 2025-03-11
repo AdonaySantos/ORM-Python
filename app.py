105b467dfb55f8099c1f6810f66f413ea7e8e1d7
Adonay <adonay.rssantos@gmail.com>
2025-03-11 20:54:36 -0300

    Criação do endpoint para inserção de dados no DB

--- app.py
+++ app.py
@@ -15,13 +15,25 @@ def get_table_schema(table_name):
 @app.route("/data/<string:table_name>", methods=['POST'])
 def get_data(table_name):
     if not request.is_json:
-        return jsonify({"error": "A requisição deve ser em formato JSON"}), 400
+        return jsonify({"error" : "A requisição deve ser em formato JSON"}), 400
     
-    data = request.json.get("columns")
+    columns = request.json.get("columns")
     
-    if not data:
-        return jsonify({"error": "Nenhuma coluna especificada"}), 400
-    return table_service.get_data(table_name, data)
+    if not columns:
+        return jsonify({"error" : "Nenhuma coluna especificada"}), 400
+    return table_service.get_data(table_name, columns)
+
+@app.route('/<string:table_name>', methods=['POST'])
+def insert_data(table_name):
+    if not request.is_json:
+        return jsonify({"error" : "A requisição deve ser em formato JSON"}), 400
+    
+    columns = request.json.get("columns")
+    data = request.json.get("data")
+    
+    if not columns or not data:
+        return jsonify({"error" : "Nenhuma coluna especificada ou nenhum dado inserido"}), 400
+    return table_service.insert_data(table_name, columns, data)
 
 if __name__ == "__main__":
     app.run(debug=True)
